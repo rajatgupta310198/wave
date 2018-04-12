a0954612df07b7350b19819ee2b933d6bce6d17e
Bapi Gupta <rajat310198@outlook.com>
2018-04-12 23:05:26 +0530

    Added play pause next stop

--- handlers.js
+++ handlers.js
@@ -24,23 +24,46 @@ for(var i = 0; i < listOfMp3s.length ; i++){
 document.body.appendChild(ul)
 
 
-var button = document.createElement("button")
-button.appendChild(document.createTextNode('Play'))
-button.addEventListener("click", function(e){
+var playBtn = document.createElement("button")
+playBtn.appendChild(document.createTextNode('Play'))
+playBtn.addEventListener("click", function(e){
     //console.log(this.innerHTML === 'Play')
-   if(this.innerHTML == "Play" ){
-    
+   if(this.innerHTML == "Play"){
+
         this.innerHTML = "Pause"
         player.play()
+
     }
-    else{
-        this.innerHTML = "Play"
-        
+    else if(this.innerHTML == "Pause"){
+        console.log('pause')
+        this.innerHTML = "Resume"
         player.pause()
+
+    }
+    else if(this.innerHTML == "Resume"){
+        console.log('resume')
+        this.innerHTML = "Pause"
+        player.resume()
+
     }
 
 })
-document.body.appendChild(button)
+
+var nextBtn = document.createElement("button")
+nextBtn.appendChild(document.createTextNode("Next"))
+nextBtn.addEventListener("click", function(e){
+    player.next()
+})
+
+var stopBtn = document.createElement("button")
+stopBtn.appendChild(document.createTextNode("Stop"))
+stopBtn.addEventListener("click", function(e){
+    player.stop()
+})
+
+document.body.appendChild(playBtn)
+document.body.appendChild(nextBtn)
+document.body.appendChild(stopBtn)
 
 
 
