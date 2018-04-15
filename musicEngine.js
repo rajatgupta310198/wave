ed5afc084d1c4fa91f20ae4bcd728ed2b297076c
Bapi Gupta <rajat310198@outlook.com>
2018-04-15 13:04:27 +0530

    prototyping for albums,  not added with main

--- musicEngine.js
+++ musicEngine.js
@@ -1,5 +1,9 @@
-let listOfMp3s = [];
+const id3 = require('node-id3')
+const fs = require("fs")
 
+
+let listOfMp3s = [];
+let dbAlbums = []
 
 function audioFile(file){
 
@@ -16,17 +20,39 @@ var walkSync = function(dir, filelist) {
 	  }
 	  else {
 		  if(audioFile(file)){
-            filelist.push({'path':dir + '/' + file, 'file':file});
+				
+				filelist.push({'path':dir + '/' + file, 'file':file})
+				
 		  }
 	  }
 	});
 	return filelist;
   };
 
+
+function addAlbumFromBuffer(buffer){
+	console.log(buffer)
+	let tags = id3.read(buffer)
+	dbAlbums.push(tags['album'])
+}
+
+function readMp3ToBuffer (songs){
+	for(i = 0; i< songs.length; i++){
+		console.log('inside loop\n')
+		let buffer = fs.readFileSync(songs[i]['path'])
+		console.log(buffer)
+		let tags = id3.read(buffer)
+		console.log(tags)
+		//addAlbumFromBuffer(buffer)
+	}
+}
+
 listOfMp3s = walkSync('/Users/bapigupta/Music', listOfMp3s);
 
-
+//readMp3ToBuffer(listOfMp3s)
+console.log(listOfMp3s.length, dbAlbums.length)
 
 module.exports.musicEngine = {
-    'listOfMp3s':listOfMp3s
+		'listOfMp3s':listOfMp3s,
+		//'albums':dbAlbums
   }
